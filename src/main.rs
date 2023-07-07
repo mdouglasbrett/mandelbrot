ceba98b920e98490e8796178f5db785a3978ec84
Michael Brett <mdouglasbrett@icloud.com>
2023-07-07 18:38:48 +0100

    Adds deps, placeholder funcs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use num::complex::Complex;
+
+fn calculate_mandelbrot() {
+    println!("Calculating!")
+}
+
+fn mandelbrot_at_point() {}
+
+fn render_mandelbrot() {
+    println!("Rendering!")
+}
 fn main() {
-    println!("Hello, world!");
+    calculate_mandelbrot();
+    render_mandelbrot();
 }
