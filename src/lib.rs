1e802371ca4a48b5cf9df58e8ef27ae4d0ac1ca1
Michael Brett <mdouglasbrett@icloud.com>
2023-07-07 19:03:22 +0100

    Adds renderer function
    This is taken from the book (emojis aside), but the compiler warns me
    of overlapping ranges...

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,29 @@
+use num::complex::Complex;
+
 fn mandelbrot_at_point() {}
 
 pub fn calculate_mandelbrot() {
     println!("Calculating!")
 }
 
+pub fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
+    for row in escape_vals {
+        let mut line = String::with_capacity(row.len());
+        for column in row {
+            let val = match column {
+                0..=2 => ' ',
+                2..=5 => '.',
+                5..=10 => '😊',
+                11..=30 => '*',
+                30..=100 => '+',
+                100..=200 => 'X',
+                200..=400 => '$',
+                400..=700 => '#',
+                _ => '🙃'
+            };
 
-pub fn render_mandelbrot() {
-    println!("Rendering!")
+            line.push(val);
+        }
+        println!("{}", line);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use mandelbrot::{calculate_mandelbrot, render_mandelbrot};
 
 fn main() {
     calculate_mandelbrot();
-    render_mandelbrot();
+    render_mandelbrot(vec![vec![20 as usize], vec![6 as usize], vec![423 as usize], vec![52 as usize]]);
 }
